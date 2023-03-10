385ee6b58a3de4df540383b2c70c9606e617bbb7
yeoni1234 <mc0403@naver.com>
2023-03-10 16:04:32 +0900

    add

---
+++ Vectors/vectors.v
@@ -0,0 +1,15 @@
+module vectors(
+
+    input wire [2:0] vec,
+    output wire [2:0] outv,
+    output wire o2, o1, o0
+
+);
+
+    assign outv = vec;
+    assign o2 = vec[2];
+    assign o1 = vec[1];
+    assign o0 = vec[0];
+
+
+endmodule
