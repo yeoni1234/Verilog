6e69819c01a63bbfca5971f50c0ecb8cae1df655
yeoni1234 <mc0403@naver.com>
2023-03-13 01:25:46 +0900

    add

--- Vectors/vectorgates.v
+++ Vectors/vectorgates.v
@@ -6,6 +6,10 @@ module vectorgates(
     output [5:0] out_not
 );
 
-
+    assign out_or_bitwise = a | b;
+    assign out_or_logical = a||b;
+    assign out_not [5:3] = ~b; // bitwise not
+    assign out_not [2:0] = ~a;
+    
 
 endmodule
