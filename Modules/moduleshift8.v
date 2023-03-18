656d0972395b474f86e05f2c18cc86c8cca59daa
yeoni1234 <mc0403@naver.com>
2023-03-19 01:31:32 +0900

    add

--- Modules/moduleshift8.v
+++ Modules/moduleshift8.v
@@ -6,6 +6,12 @@ module moduleshift8(
     output [7:0] q
 );
 
+    wire[7:0] q1;
+    wire[7:0] q2;
+
+    my_dff instance1 ( .clk(clk), .d(d), .q(q1));
+    my_dff instance2 ( .clk(clk), .d(q1), .q2(q2));
+    my_dff instance3 ( .clk(clk), .q2(q2), .q(q));
 
 
 endmodule
